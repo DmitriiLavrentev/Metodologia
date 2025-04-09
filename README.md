b6a85021ba8ea4409591e62545da97088142d75b
DmitriiLavrentev <m2211227@edu.misis.ru>
2025-04-09 12:45:23 +0300

    new_engine

--- README.md
+++
@@ -1,5 +0,0 @@
-https://asciinema.org/a/exIyHTCXNfln7RVQtLCGIdY1q
-
-https://asciinema.org/a/r7Ltu1rLMs6wvVl9pQDvhoRNO
-
-[![Maintainability](https://api.codeclimate.com/v1/badges/e47e28f003f50228d358/maintainability)](https://codeclimate.com/github/DmitriiLavrentev/Metodologia/maintainability)
--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-from games.game1 import play_game as play_game_1
-from games.game2 import play_game as play_game_2
-from engine.game_engine import run_game
+from games.lcm import play_lcm
+from games.progression import play_progression
+
+
+def main():
+    games = {
+        "1": ("LCM Game", play_lcm),
+        "2": ("Geometric Progression", play_progression),
+    }
+
+    print("Choose a game:")
+    for key, (name, _) in games.items():
+        print(f"{key} - {name}")
+
+    choice = input("Enter choice: ")
+
+    if choice in games:
+        _, game_function = games[choice]
+        game_function()
+    else:
+        print("Invalid choice!")
+
 
 if __name__ == "__main__":
-    print("Welcome to the games!")
-    while True:
-        print("Choose a game:")
-        print("1. Game 1 (Find the smallest common multiple)")
-        print("2. Game 2 (Find the missing number in the progression)")
-        print("0. Exit")
-
-        choice = input("Your choice: ")
-
-        if choice == "1":
-            run_game(play_game_1)
-        elif choice == "2":
-            run_game(play_game_2)
-        elif choice == "0":
-            print("Goodbye!")
-            break
-        else:
-            print("Invalid choice, please try again.")
+    main()
